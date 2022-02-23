b0a664bd85073ff5b12dbf3e049aa722830d4c89
stefkudla <stefankudla@gmail.com>
2022-02-23 15:09:03 -0800

    <added CTA Modal>

--- script.js
+++ script.js
@@ -7,15 +7,33 @@
 const learnMoreBtn = document.querySelector('.learn-button');
 const mobileMenuBtn = document.getElementById('menu--button');
 const closeMenuBtn = document.getElementById('close--button');
+const ctaBtn = document.querySelector('.cta-button');
+const mobileNavigation = document.querySelector('#mobile-navigation');
 const mainNav = document.querySelector('.nav--items');
-const mobileNav = document.querySelector('.mobile-nav--items');
+const mobileNavLinks = document.querySelector('.mobile-nav--items');
+const CtaForm = document.querySelector('.cta-form-card-container');
+
+//     =============
+
+//     Function: Navigation Menus
+
+//     =============
 
 function toggleMobileNav() {
-  let element = document.getElementById('mobile-navigation');
-
-  element.classList.toggle('mobile-navigation__open');
+  mobileNavigation.classList.toggle('mobile-navigation__open');
   document.querySelector('#container').classList.toggle('dim');
 }
+
+window.addEventListener('click', function (e) {
+  if (!mobileNavigation.classList.contains('mobile-navigation__open')) return;
+  if (document.querySelector('.fa-reorder').contains(e.target)) return;
+  if (!mobileNavigation.contains(e.target)) {
+    toggleMobileNav();
+  }
+});
+
+mobileMenuBtn.onclick = () => toggleMobileNav();
+closeMenuBtn.onclick = () => toggleMobileNav();
 
 function setActive(currentNav) {
   aObj = currentNav.getElementsByClassName('nav--item');
@@ -26,11 +44,36 @@ function setActive(currentNav) {
     }
   }
 }
-setActive(mobileNav);
+setActive(mobileNavLinks);
 setActive(mainNav);
 
-mobileMenuBtn.onclick = () => toggleMobileNav();
-closeMenuBtn.onclick = () => toggleMobileNav();
+//     =============
+
+//     Function: CTA-Buttons
+
+//     =============
+
+function openCtaModal() {
+  CtaForm.classList.add('show-modal');
+  document.querySelector('#container').classList.toggle('dim');
+  document.body.classList.add('fixed');
+}
+
+function closeCtaModal() {
+  CtaForm.classList.remove('show-modal');
+  document.querySelector('#container').classList.toggle('dim');
+  document.body.classList.remove('fixed');
+}
+
+// window.addEventListener('click', function (e) {
+//   // if (!CtaForm.classList.contains('show-modal')) console.log('ok');
+//   if (
+//     CtaForm.classList.contains('show-modal') &&
+//     !document.querySelector('.cta-form').contains(e.target)
+//   ) {
+//     closeCtaModal();
+//   }
+// });
 
 function smoothScroll() {
   learnMoreBtn.onclick = () => {
@@ -40,3 +83,4 @@ function smoothScroll() {
   };
 }
 document.location.pathname === '/index.html' && smoothScroll();
+document.location.pathname === '/' && smoothScroll();
